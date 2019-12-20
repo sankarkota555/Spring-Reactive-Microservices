0054af166a293dc240e26d3076700f1c219e23fc
shankar <sankar.kota555@gmail.com>
2019-12-20 18:13:21 +0530

    fix issue

--- Auth-Libraries/src/main/java/com/reactive/app/authentication/common/config/OAuth2CommonConfig.java
+++ Auth-Libraries/src/main/java/com/reactive/app/authentication/common/config/OAuth2CommonConfig.java
@@ -6,17 +6,17 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 
 import com.reactive.app.authentication.constants.AuthenticationConstants;
-import com.reactive.app.authentication.tokenstore.TapasMongoTokenStore;
+import com.reactive.app.authentication.tokenstore.CustomMongoTokenStore;
 
 @Configuration
 public class OAuth2CommonConfig {
 
 	@Primary
 	@Bean
-	public DefaultTokenServices tokenServices(TapasMongoTokenStore tapasMongoTokenStore) {
+	public DefaultTokenServices tokenServices(CustomMongoTokenStore mongoTokenStore) {
 		DefaultTokenServices tokenServices = new DefaultTokenServices();
 		tokenServices.setSupportRefreshToken(true);
-		tokenServices.setTokenStore(tapasMongoTokenStore);
+		tokenServices.setTokenStore(mongoTokenStore);
 		tokenServices.setAccessTokenValiditySeconds(AuthenticationConstants.ACCESS_TOKEN_EXPIRY_TIME);
 		tokenServices.setRefreshTokenValiditySeconds(AuthenticationConstants.REFRESH_TOKEN_EXPIRY_TIME);
 		return tokenServices;
